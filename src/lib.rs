e55181ce51bed500895da1ccc564f492e18468a4
AvraamMavridis <avraammauridis@gmail.com>
2018-12-15 22:05:32 +0100

    Feat: Get css rules

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,54 @@
-use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::*;
+use js_sys::*;
+
+#[wasm_bindgen]
+extern "C" {
+    // Use `js_namespace` here to bind `console.log(..)` instead of just
+    // `log(..)`
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    // The `console.log` is quite polymorphic, so we can bind it with multiple
+    // signatures. Note that we need to use `js_name` to ensure we always call
+    // `log` in JS.
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
+    // Multiple arguments too!
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
+
+
+fn get_css_rules(document: &web_sys::Document) -> Vec<web_sys::CssRule> {
+    let stylesheets: Array = Array::from(&document.style_sheets());
+    let mut document_css_rules = Vec::new();
+
+    stylesheets.for_each(&mut |style, _, _| {
+        let css_style_sheet = CssStyleSheet::from(JsValue::from(style));
+        let rules = Array::from(&css_style_sheet.css_rules().unwrap());
+
+        rules.for_each(&mut |rule, _, _| {
+            document_css_rules.push(web_sys::CssRule::from(rule));
+        });
+    });
+
+    return document_css_rules;
+}
+
+fn get_font_face_rules(rules: &Vec<web_sys::CssRule>) -> Vec<&web_sys::CssRule> {
+    let mut font_rules = Vec::new();
+
+   rules.iter().for_each(|x| {
+       if(x.type_() == web_sys::CssRule::FONT_FACE_RULE) {
+           font_rules.push(x);
+       }
+   });
+
+    return font_rules;
+}
 
 // Export a `greet` function from Rust to JavaScript, that alerts a
 // hello message.
@@ -23,6 +70,14 @@ pub fn draw(node: &web_sys::HtmlElement) {
     _svg.set_attribute("width", &width);
     _svg.set_attribute("height", &height);
     _svg.set_attribute("viewBox",  &viewbox);
+
+
+    log("Take document styless");
+    let rules = get_css_rules(&document);
+    get_font_face_rules(&rules);
+
+    log(&rules.len().to_string());
+
 
     body.append_child(&_svg);
 
